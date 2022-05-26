cc6e4274180159975f5ee0482b7670261509e49b
kiri0314 <t.kirihara0314@gmail.com>
2022-05-26 23:53:49 +0900

    fix: argvを直接いじらないように修正

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,11 +6,12 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 02:22:05 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/26 03:00:09 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/05/26 23:53:07 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int	main(int ac, char **av)
 {
@@ -22,12 +23,13 @@ int	main(int ac, char **av)
 	for (int i = 1; i < ac; i++)
 	{
 		size_t	j = 0;
+		std::string s = "";
 		while (av[i][j] != '\0')
 		{
-			av[i][j] = toupper(av[i][j]);
+			s += toupper(av[i][j]);
 			j++;
 		}
-		std::cout << av[i];
+		std::cout << s;
 	}
 	std::cout << std::endl;
 	return 0;
