38d78febfc961a4b846f7586e888710f6619163f
kiri0314 <t.kirihara0314@gmail.com>
2022-05-27 09:37:56 +0900

    ref: タブをスペースに変換

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 16:23:16 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/05/27 09:29:34 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/05/27 09:36:05 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,27 @@
 
 void	PhoneBook::repl()
 {
-	std::string command;
-	while (1)
-	{
-		std::cout << F_YELLOW;
-		std::cout << "Enter the command(ADD SEARCH EXIT)" << std::endl;
-		std::cout << F_RESET;
+  std::string command;
+  while (1)
+  {
+    std::cout << F_YELLOW;
+    std::cout << "Enter the command(ADD SEARCH EXIT)" << std::endl;
+    std::cout << F_RESET;
 
-		command = getline();
-		if (command == "ADD")
-			add();
-		else if (command == "SEARCH")
-			search();
-		else if (command == "EXIT")
-			break ;
-		else
-		{
-			std::cout << F_RED;
-			std::cout << "The command name is wrong" << std::endl;
-			std::cout << F_RESET;
-		}
-	}
+    command = getline();
+    if (command == "ADD")
+      add();
+    else if (command == "SEARCH")
+      search();
+    else if (command == "EXIT")
+      break ;
+    else
+    {
+      std::cout << F_RED;
+      std::cout << "The command name is wrong" << std::endl;
+      std::cout << F_RESET;
+    }
+  }
 }
 
 std::string PhoneBook::getline()
@@ -63,58 +63,58 @@ std::string PhoneBook::getline()
 
 void	PhoneBook::add()
 {
-	size_t	phone_book_number;
-	phone_book_number = add_count % 8;
-	contactlst[phone_book_number].insert();
-	add_count++;
-	return ;
+  size_t	phone_book_number;
+  phone_book_number = add_count % 8;
+  contactlst[phone_book_number].insert();
+  add_count++;
+  return ;
 }
 
 void	PhoneBook::search()
 {
-	if (add_count == 0)
-	{
-		std::cout << F_RED << "There is no data" << F_RESET << std::endl;
-		return ;
-	}
-	size_t	max_size = 8;
-	if (max_size > add_count)
-	{
-		max_size = add_count;
-	}
-	show_heading();
-	for (size_t i = 0; i < max_size; i++)
-	{
-		contactlst[i].show_list(i);
-	}
-	if (add_count != 0)
-	{
-		size_t	index;
-		while (1)
-		{
-			std::cout << F_YELLOW << "Enter the index(1 ~ 8)" << F_RESET << std::endl;
-			std::cin >> index;
-			index--;
-			if (0 <= index && index < max_size)
-			{
-				contactlst[index].show_person();
-				break ;
-			}
-			std::cout << F_RED;
-			std::cout << "The value is wrong" << std::endl;
-			std::cout << "Enter the correct value" << std::endl;
-			std::cout << F_RESET;
-		}
-	}
-	return ;
+  if (add_count == 0)
+  {
+    std::cout << F_RED << "There is no data" << F_RESET << std::endl;
+    return ;
+  }
+  size_t	max_size = 8;
+  if (max_size > add_count)
+  {
+    max_size = add_count;
+  }
+  show_heading();
+  for (size_t i = 0; i < max_size; i++)
+  {
+    contactlst[i].show_list(i);
+  }
+  if (add_count != 0)
+  {
+    size_t	index;
+    while (1)
+    {
+      std::cout << F_YELLOW << "Enter the index(1 ~ 8)" << F_RESET << std::endl;
+      std::cin >> index;
+      index--;
+      if (0 <= index && index < max_size)
+      {
+        contactlst[index].show_person();
+        break ;
+      }
+      std::cout << F_RED;
+      std::cout << "The value is wrong" << std::endl;
+      std::cout << "Enter the correct value" << std::endl;
+      std::cout << F_RESET;
+    }
+  }
+  return ;
 }
 
 void	PhoneBook::show_heading()
 {
-	std::cout << "|" << std::setw(10) << std::right << "INDEX";
-	std::cout << "|" << std::setw(10) << std::right << "FIRST NAME";
-	std::cout << "|" << std::setw(10) << std::right << "LAST NAME";
-	std::cout << "|" << std::setw(10) << std::right << "NICK NAME";
-	std::cout << "|" << std::endl;
-	return ;
+  std::cout << "|" << std::setw(10) << std::right << "INDEX";
+  std::cout << "|" << std::setw(10) << std::right << "FIRST NAME";
+  std::cout << "|" << std::setw(10) << std::right << "LAST NAME";
+  std::cout << "|" << std::setw(10) << std::right << "NICK NAME";
+  std::cout << "|" << std::endl;
+  return ;
 }
